74cc528b01dae73335a43430382af76290ba2692
Brian Walsh <brian@bwalsh.com>
2019-10-28 10:13:00 -0700

    Adds eMERGE (wip)

---
+++ pyAnVIL/anvil/transformers/eMERGE.py
@@ -0,0 +1,9 @@
+from . import BaseApp
+
+
+class eMERGE(BaseApp):
+    """Transforms eMERGE to cannonical graph."""
+
+    def __init__(self, project_pattern='^.*_eMERGE_.*$', *args, **kwargs):
+        """Initializes class variables."""
+        super(eMERGE, self).__init__(project_pattern=project_pattern, **kwargs)
