0b954eda0c805e5a6ac678a8b4437b867c1f1e31
Brian Walsh <walsbr@ohsu.edu>
2021-04-08 14:14:17 -0700

    Incorporates demo feedback #35, #36

--- pyAnVIL/setup.py
+++ pyAnVIL/setup.py
@@ -52,7 +52,7 @@ setup(
 
     # Versions should comply with PEP 440:
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.7rc2',  # Required
+    version='0.0.7rc6',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
